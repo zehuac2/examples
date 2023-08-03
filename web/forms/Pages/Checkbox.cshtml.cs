0f7aa06faa74a4071c5e58d883de9e3e99f6a0f9
Zehua Chen <zehua-chen@outlook.com>
2023-08-03 01:24:41 -0700

    [feat] Add checkbox example

---
+++ web/forms/Pages/Checkbox.cshtml.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Forms.Pages
+{
+    public class CheckboxModel : PageModel
+    {
+        public enum Attribute
+        {
+            Fast,
+            Slow,
+            Male,
+            Female
+        }
+
+        [BindProperty]
+        public Attribute[] AvailableAttributes
+        {
+            get
+            {
+                return Enum.GetValues<Attribute>();
+            }
+        }
+
+        [BindProperty]
+        public string Message
+        {
+            get
+            {
+                if (_selectedAttributes.Count == 0)
+                {
+                    return "Nothing selected";
+                }
+
+                return string.Join(",", _selectedAttributes);
+            }
+        }
+
+        [BindProperty]
+        public List<Attribute> SelectedAttributes
+        {
+            get
+            {
+                return _selectedAttributes.ToList();
+            }
+            set
+            {
+                _selectedAttributes = new HashSet<Attribute>(value);
+            }
+        }
+
+        private HashSet<Attribute> _selectedAttributes = new HashSet<Attribute>();
+
+        public void OnGet()
+        {
+            _selectedAttributes = new HashSet<Attribute>()
+            {
+                Attribute.Fast
+            };
+        }
+
+        public bool IsAttributeSelected(Attribute attribute)
+        {
+            return _selectedAttributes.Contains(attribute);
+        }
+
+        public void OnPostSubmit()
+        {
+        }
+    }
+}
