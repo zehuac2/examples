ba6a89c92c199be07de193c3e1d3eb81a8f7883a
Zehua Chen <zehua-chen@outlook.com>
2023-08-03 23:19:14 -0700

    [feat] Improve index page

--- web/forms/Pages/Index.cshtml.cs
+++ web/forms/Pages/Index.cshtml.cs
@@ -1,10 +1,23 @@
-﻿using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.RazorPages;
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace Forms.Pages;
 
 public class IndexModel : PageModel
 {
+    public record struct ExamplesModel(string Url, string Name);
+
+    public ExamplesModel[] Examples
+    {
+        get
+        {
+            return new ExamplesModel[]
+            {
+                new ExamplesModel("AddAndDelete", "Add and delete"),
+                new ExamplesModel("Checkbox", "Checkbox"),
+            };
+        }
+    }
+    
     private readonly ILogger<IndexModel> _logger;
 
     public IndexModel(ILogger<IndexModel> logger)
